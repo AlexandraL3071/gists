59f4deb19d8468784b37f51f7e83bcdfe47e8cad
AlexandraL3071 <alexandra_lakatos15@yahoo.com>
2021-08-15 14:12:28 +0300

    Create GistItem.vue
    Create GistItem component to display the id and files of a gist

---
+++ src/components/GistItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ id }}:</h3>
+    <ol>
+      <li v-for="file in files" :key="file.filename">{{ file.filename }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    files: [],
+  },
+};
+</script>
+
+<style scope>
+h3 {
+  color: red;
+}
+</style>
