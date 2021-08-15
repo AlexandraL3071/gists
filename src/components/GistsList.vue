60613854f6ef54f76d65f57fc746d3a074c633c4
AlexandraL3071 <alexandra_lakatos15@yahoo.com>
2021-08-15 13:08:40 +0300

    Update GistsList.vue
    Create a GistsList component for the gists of the user - initial form with some dummy code (it displays for now the usename which has been entered)

--- src/components/GistsList.vue
+++ src/components/GistsList.vue
@@ -1,10 +1,17 @@
 <template>
-  <p>Hello</p>
+  <h2>{{ enteredUsername }}</h2>
 </template>
 
 <script>
 export default {
-
+  props: {
+    enteredUsername: String,
+  },
+  watch: {
+    enteredUsername(newValue) {
+      console.log("In GistsList.vue: ", newValue);
+    },
+  },
 };
 </script>
 
