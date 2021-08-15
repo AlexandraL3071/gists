1a4d0bddf41bbc230c51ec019679ff72db12dee0
AlexandraL3071 <alexandra_lakatos15@yahoo.com>
2021-08-15 19:00:28 +0300

    Update GistItem component
    Update the GistItem component so that:
    - it uses the GistFile component for rendering all containing files
    - make an axios request at component creation to get the forks for the gist
    - write logic for determining the last users who forked the gist

--- src/components/GistItem.vue
+++ src/components/GistItem.vue
@@ -1,17 +1,78 @@
 <template>
   <div>
     <h3>{{ id }}:</h3>
-    <ol>
-      <li v-for="file in files" :key="file.filename">{{ file.filename }}</li>
-    </ol>
+    <gist-file
+      v-for="file in files"
+      :key="file.filename"
+      :language="file.language"
+      :filename="file.filename"
+      :content-url="file.raw_url"
+      >{{ file.filename }}</gist-file
+    >
+    <p v-if="usersWhoForked !== ''">
+      The last 3 users who forked it: {{ usersWhoForked }}
+    </p>
   </div>
 </template>
 
 <script>
+import GistFile from "./GistFile.vue";
+
 export default {
   props: {
     id: String,
-    files: [],
+    files: Object,
+  },
+  data() {
+    return {
+      usersWhoForked: "",
+      forks: [],
+    };
+  },
+  methods: {
+    fetchForks() {
+      const axios = require("axios");
+      axios
+        .get("https://api.github.com/gists/" + this.id + "/forks")
+        .then((response) => {
+          if (response.data) {
+            this.forks = response.data;
+            this.computeLastForksUsers();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    computeLastForksUsers() {
+      this.forks = this.forks.reverse();
+      var necessaryNumberOfForks = 0;
+      var users = [];
+      for (
+        var i = 0;
+        i < this.forks.length && necessaryNumberOfForks < 3;
+        i++
+      ) {
+        users.push(this.forks[i].owner.login);
+        necessaryNumberOfForks++;
+      }
+      var usersStringValue = "";
+      for (var j = 0; j < users.length - 1; j++) {
+        usersStringValue += users[j] + ", ";
+      }
+
+      if (users.length - 1 >= 0) {
+        usersStringValue += users[users.length - 1];
+      }
+
+      this.usersWhoForked = usersStringValue;
+    },
+  },
+  components: {
+    GistFile,
+  },
+  created() {
+    this.fetchForks();
   },
 };
 </script>
