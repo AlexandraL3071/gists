bb06c7bb4dbb265311c4ff8d4e2b63e075ef21dd
alexandral1221 <alexandra_lakatos15@yahoo.com>
2021-08-15 11:40:19 +0300

    Initial contribution - setup the Vue js app with some dummy code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <gists-list></gists-list>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GistsList from "./components/GistsList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    GistsList,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
