ba60523f2b55ed4e4b42e284256f2f15bf96aedd
AlexandraL3071 <alexandra_lakatos15@yahoo.com>
2021-08-15 13:07:18 +0300

    Update App.vue
     Update the main App component so that:
    - it listens for the event emitted in the SearchBar component so that it receives the entered username and stores it
    - it passes the username down to the GistsList component (which will be responsible for fetching the gists of the username)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <gists-list></gists-list>
+  <search-bar @enter-username="usernameEntered"></search-bar>
+  <gists-list :entered-username="username"></gists-list>
 </template>
 
 <script>
 import GistsList from "./components/GistsList.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    usernameEntered(username) {
+      this.username = username;
+    }
+  },
   components: {
     GistsList,
+    SearchBar,
   },
 };
 </script>
